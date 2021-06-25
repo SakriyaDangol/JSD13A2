4ecb73af47358ba541377bbde74d251ceea50d9b
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-25 14:35:58 +0545

    updated

--- script.js
+++ script.js
@@ -2,9 +2,8 @@ window.addEventListener('keydown', soundPlay);
 
 function soundPlay(event){
 
-    const sound = document.getElementById(event.which);    
+    const sound = document.getElementById(event.which).play();
     console.log(sound);
 
     sound.currentTime = 0;
-    sound.play();
 }
